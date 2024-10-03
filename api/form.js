d59ea62feb21a76a395ec0533a7631ef4ab9b0c0
Kelvin-BearPlus <tranquangkhanh2k2ute@gmail.com>
2024-10-03 10:28:40 +0700

    update

--- api/form.js
+++ api/form.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const formRoutes = require('../routes/routerForm');
 
 const app = express();
-const port = 3000;
 
 // Middleware để parse body
 app.use(bodyParser.json());
@@ -11,6 +10,5 @@ app.use(bodyParser.json());
 // Định tuyến cho form
 app.use('/api/forms', formRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// Xuất `app` để Vercel sử dụng như một serverless function
+module.exports = app;
