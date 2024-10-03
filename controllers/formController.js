15f716043e14cc7ec5900940a7a0a09194cf9f13
Kelvin-BearPlus <tranquangkhanh2k2ute@gmail.com>
2024-10-03 22:33:50 +0700

    Update formController.js

--- controllers/formController.js
+++ controllers/formController.js
@@ -2,10 +2,22 @@ const pool = require('../config/db');
 
 // Hàm lấy tất cả các forms
 exports.getAllForms = async (req, res) => {
-  const query = 'SELECT * FROM formsubmit';
+  // Lấy formName từ query parameters
+  const { formName } = req.query;
+  
+  // Tạo câu truy vấn SQL, nếu có formName thì thêm điều kiện WHERE để lọc
+  let query = 'SELECT * FROM formsubmit';
+  const queryParams = [];
+
+  if (formName) {
+    query += ' WHERE form_name = $1'; // Sử dụng parameterized query để tránh SQL injection
+    queryParams.push(formName);
+  }
   
   try {
-    const results = await pool.query(query);
+    // Thực hiện truy vấn với điều kiện lọc nếu có
+    const results = await pool.query(query, queryParams);
+    
     // Trả về dữ liệu dưới dạng JSON
     res.status(200).json({ success: true, data: results.rows });
   } catch (err) {
@@ -13,6 +25,7 @@ exports.getAllForms = async (req, res) => {
     return res.status(500).json({ success: false, message: 'Internal Server Error' });
   }
 };
+
 
 // Hàm submit form
 exports.submitForm = async (req, res) => {
